99c14cbbd265c410311817bd91ccdef37b1a2b20
CCnova <caiocnova1@gmail.com>
2019-10-14 10:50:35 -0300

    Add the PopulationFormatter code and improves the output

--- Program.cs
+++ Program.cs
@@ -14,7 +14,8 @@ namespace ArrayCollectionsPopByLargest
 
             foreach(Country country in countries)
             {
-                Console.WriteLine($"{country.Population}: {country.Name}");
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+
             }
         }
     }
