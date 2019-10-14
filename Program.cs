d6e5994f310a1896e7cb2d6a0c66d5547bf181d1
CCnova <caiocnova1@gmail.com>
2019-10-14 10:42:19 -0300

    Added the code to list the top 10 most populated countries

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ArrayCollectionsPopByLargest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\CNova\Documents\Estagio\Curso Pluralsight C#\csharp-collections-beginning\Pop by Largest Final.csv";
+
+            CsvReader reader = new CsvReader(filePath);
+
+            Country[] countries = reader.ReadFirstNCountries(10);
+
+            foreach(Country country in countries)
+            {
+                Console.WriteLine($"{country.Population}: {country.Name}");
+            }
+        }
+    }
+}
